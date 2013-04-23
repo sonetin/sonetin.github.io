e910d3819fee927eb9c1bad685fd30173515c8de
Arnaud Leymet <arnaud.leymet@gmail.com>
2013-04-23 15:17:08 +0200

    Init

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   var href = document.location.pathname.split('/').reverse()[0];
   $('.navbar li a[href $= "' + href + '"], footer li a[href $= "' + href + '"]').parent().addClass('active');
-  if(href == '') {
-    $('.navbar li:first-child').addClass('active');
-  }
+//  if(href == '') {
+//    $('.navbar li:first-child').addClass('active');
+//  }
 });
