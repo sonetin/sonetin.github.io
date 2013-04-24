b9f696fc077dc48c7a67dbf859686b09e7ae2f66
Arnaud Leymet <arnaud.leymet@gmail.com>
2013-04-24 12:19:26 +0200

    Init

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
-  var href = document.location.pathname.split('/').reverse()[0];
-  $('.navbar li a[href $= "' + href + '"], footer li a[href $= "' + href + '"]').parent().addClass('active');
-//  if(href == '') {
-//    $('.navbar li:first-child').addClass('active');
-//  }
+  $('.navbar .nav a').on('click', function(e) {
+    $('html, body').animate({ scrollTop: $("#features").offset().top }, 500);
+    return false;
+  });
 });
