bf8d0cd9275861e7409417c454dda1ac3d778c7f
Arnaud Leymet <arnaud.leymet@gmail.com>
2013-06-12 09:29:42 +0200

    Tiny update of min.'s

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(function() {
   $('.navbar .nav a').on('click', function(e) {
     $('html, body').animate({ scrollTop: $("#features").offset().top }, 500);
     return false;
