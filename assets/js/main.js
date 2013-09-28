45956a784301576397e7bcb297d8a6b4134751f7
Arnaud Leymet <arnaud.leymet@gmail.com>
2013-09-28 15:14:11 +0200

    Revamped website

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,59 @@
-$(function() {
-  $('.navbar .nav a').on('click', function(e) {
-    $('html, body').animate({ scrollTop: $("#features").offset().top }, 500);
+function goto(element, func) {
+  $('html, body').animate({ scrollTop: $('#'+$(element).attr('href').split('#')[1]).offset().top }, 500, func);
+  return false;
+}
+
+function booking_check() {
+  var tenant_name = $('#tenant_name').val();
+  if($.inArray(tenant_name, ['', 'www', 'api']) != -1) {
+    //$('#tenant_name').parents('form-group').addClass('has-error');
+    $('#tenant_name').focus();
     return false;
+  }
+  //$.post('https://api.sonetin.com/v1/tenants/check',
+  //  { tenant_name: $('#tenant_name').val() },
+  //  function() {
+      //$('#tenant_name').parents('form-group').addClass('has-success');
+      $('#booking-check').fadeOut('normal', function() {
+        $('#booking-account').removeClass('hide').fadeIn('normal');
+      });
+  //  }
+  //);
+  return false;
+}
+
+function change_tenant_name() {
+  $('#tenant_name').val('');
+  $('#booking-account').fadeOut('normal', function() {
+    $('#booking-check').fadeIn('normal', function() {
+      $('#tenant_name').focus();
+    });
   });
+}
+
+function booking_create() {
+  var endpoint = 'https://api.sonetin.com/v1/tenants/create';
+  endpoint = $("#booking form").attr('action');
+  $.post(endpoint,
+    $("#booking form").serialize(),
+    function(data) {
+    }
+  );
+  $('#booking-account').fadeOut('normal', function() {
+    $('#booking-success').removeClass('hide').fadeIn('normal');
+  });
+  return false;
+}
+
+$(function() {
+  $('.action-book').on('click', function(e) {
+    return goto(this, function() { $('#tenant_name').focus() });
+  });
+  $('a.scrollable-link').on('click', function(e) {
+    return goto(this);
+  });
+  $('#booking-check-submit').on('click', booking_check);
+  $('#booking-account-submit').on('click', booking_create);
+
+  $('input, textarea').placeholder();
 });
