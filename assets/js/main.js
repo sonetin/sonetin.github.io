6f46693a60e296e950e6601fae9ad1b7e7abe546
Arnaud Leymet <arnaud.leymet@gmail.com>
2014-06-03 15:47:01 +0200

    Clearer A

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,15 @@ function goto(element, func) {
   return false;
 }
 
-function currentPlan() {
-  return document.getElementById('booking_plan').value;
+function formValues() {
+  return $("#booking").serializeArray().reduce(function(obj, item) {
+    obj[item.name] = item.value;
+    return obj;
+  }, {});
 }
 
 function booking_check() {
-  analytics.track('Booking check', { plan: currentPlan() });
+  analytics.track('Booking check', formValues());
   var tenant_name = $('#tenant_name').val();
   var tenant_name_valid = tenant_name.search(new RegExp($('#tenant_name').attr('pattern'))) >= 0;
   if($.inArray(tenant_name, ['', 'www', 'api']) != -1) {
@@ -24,14 +27,14 @@ function booking_check() {
 }
 
 function booking_error() {
-  analytics.track('Booking error', { plan: currentPlan() });
+  analytics.track('Booking error', formValues());
   $('#tenant_name').focus().parents('.form-group').addClass('has-error');
   $('#tenant_name').tooltip({trigger: 'manual', placement: 'bottom', title: $('#tenant_name').data('error-msg')}).tooltip('show');
   return false;
 }
 
 function booking_success(data) {
-  analytics.track('Booking available', { plan: currentPlan() });
+  analytics.track('Booking available', formValues());
   if(data['exists']) return booking_error();
   $('#tenant_name').tooltip('hide');
   $('#tenant_name').parents('.form-group').removeClass('has-error');
@@ -41,7 +44,7 @@ function booking_success(data) {
 }
 
 function change_tenant_name() {
-  analytics.track('Booking change', { plan: currentPlan() });
+  analytics.track('Booking change', formValues());
   $('#tenant_name').val('');
   $('#booking-account').fadeOut('normal', function() {
     $('#booking-check').fadeIn('normal', function() {
@@ -51,21 +54,31 @@ function change_tenant_name() {
 }
 
 function booking_error2() {
-  analytics.track('Booking error2', { plan: currentPlan() });
+  analytics.track('Booking error2', formValues());
   $('#booking-account-submit').tooltip({trigger: 'manual', placement: 'bottom', title: $('#booking').data('error-msg')}).tooltip('show');
   return false;
+}
+
+function analyticsIdentify() {
+  var tenant_name = $('#tenant_name').val().toLowerCase(),
+      firstName = $('#firstname').val(),
+      lastName = $('#lastname').val(),
+      email = $('#email').val();
+  analytics.identify(tenant_name+"#"+email, {
+    email: email,
+    firstName: firstName,
+    lastName: lastName
+  });
 }
 
 function booking_create() {
   if(!$('#booking').get(0).checkValidity()) {
     return booking_error2();
   }
-  analytics.track('Signed Up', { plan: currentPlan() });
+  analytics.track('Signed Up', formValues());
+  analyticsIdentify();
   var endpoint = $("#booking").attr('action');
-  $.post(endpoint,
-    $("#booking").serialize(),
-    function(data) {}
-  );
+  $.post(endpoint, $("#booking").serialize());
   $('#booking-account').fadeOut('normal', function() {
     $('#booking-success').removeClass('hide').fadeIn('normal');
   });
